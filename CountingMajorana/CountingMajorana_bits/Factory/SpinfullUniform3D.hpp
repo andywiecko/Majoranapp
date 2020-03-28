18fde20a00c19c478fbb202e33bd9e7e374c2d31
andywiecko <andywiecko@gmail.com>
2020-03-28 14:46:52 +0100

    Spinfulluniform3d added

---
+++ CountingMajorana/CountingMajorana_bits/Factory/SpinfullUniform3D.hpp
@@ -0,0 +1,120 @@
+#ifndef FACTORY_SPINFULLUNIFORM3D_HPP
+#define FACTORY_SPINFULLUNIFORM3D_HPP
+
+#include "../Hamiltonian.hpp"
+#include "../Parameters.hpp"
+#include "../Dimensions.hpp"
+#include "../Filler.hpp"
+
+/**
+ * @brief Spinfull 3D cuboid with open boundary conditions
+ * 
+ * Dimensions:
+ * - Length (L)
+ * - Width  (W)
+ * - Height (H)
+ * 
+ * Parameters:
+ * - Non-local terms:
+ *      - Spinfull::KineticTerm
+ *      - Spinfull::RashbaXTerm
+ *      - Spinfull::RashbaYTerm
+ *      - Spinfull::RashbaZTerm
+ * - Local terms:
+ *      - Spinfull::ProxTerm
+ *      - Spinfull::ChemicalTerm
+ *      - Spinfull::ZeemanXTerm
+ *      - Spinfull::ZeemanYTerm
+ *      - Spinfull::ZeemanZTerm
+ */
+class SpinfullUniform3D
+{
+public:
+    template <class T>
+    static Hamiltonian<T> Generate(Dimensions &dimensions, Parameters &parameters)
+    {
+        int deg = 4;
+        int length = dimensions.GetLength();
+        int width = dimensions.GetWidth();
+        int height = dimensions.GetHeight();
+        int N = length * width * height;
+
+        Hamiltonian<T> ham(N, deg);
+
+        // local terms
+        for (int i = 0; i < N; i++)
+        {
+            Filler<Spinfull::ChemicalTerm>::Fill(ham, parameters, i);
+            Filler<Spinfull::ProxTerm>::Fill(ham, parameters, i);
+            Filler<Spinfull::ZeemanXTerm>::Fill(ham, parameters, i);
+            Filler<Spinfull::ZeemanYTerm>::Fill(ham, parameters, i);
+            Filler<Spinfull::ZeemanZTerm>::Fill(ham, parameters, i);
+        }
+
+        // x-direction links
+        for (int z = 0; z < height; z++)
+        {
+            for (int y = 0; y < width; y++)
+            {
+                for (int x = 0; x < length - 1; x++)
+                {
+                    int from = ToSite(x, y, z, length, int width);
+                    int to = ToSite(x + 1, y, z, length, int width);
+                    //std::cout << "(" << from << ", " << to << ")" << std::endl;
+                    Filler<Spinfull::KineticTerm>::Fill(ham, parameters, from, to);
+                    Filler<Spinfull::RashbaYTerm>::Fill(ham, parameters, from, to);
+                }
+            }
+        }
+
+        //std::cout << std::endl;
+
+        // y-direction links
+        for (int z = 0; z < height; z++)
+        {
+            for (int x = 0; x < length; x++)
+            {
+                for (int y = 0; y < width - 1; y++)
+                {
+                    int from = ToSite(x, y, z, length, int width);
+                    int to = ToSite(x, y + 1, z, length, int width);
+                    //std::cout << "(" << from << ", " << to << ")" << std::endl;
+                    Filler<Spinfull::KineticTerm>::Fill(ham, parameters, from, to);
+                    Filler<Spinfull::RashbaXTerm>::Fill(ham, parameters, from, to);
+                }
+            }
+        }
+
+        // z-direction links
+        for (int y = 0; y < width; y++)
+        {
+            for (int x = 0; x < length; x++)
+            {
+                for (int z = 0; z < height - 1; z++)
+                {
+                    int from = ToSite(x, y, z, length, int width);
+                    int to = ToSite(x, y, z + 1, length, int width);
+                    //std::cout << "(" << from << ", " << to << ")" << std::endl;
+                    Filler<Spinfull::KineticTerm>::Fill(ham, parameters, from, to);
+                    Filler<Spinfull::RashbaXTerm>::Fill(ham, parameters, from, to);
+                }
+            }
+        }
+
+        return ham;
+    }
+
+private:
+    int ToSite(int x, int y, int z, int L, int W)
+    {
+        return x + L * y + L * W * z;
+    }
+    void ToCoord(int i, int &x, int &y, int &z, int L, int W)
+    {
+        x = i % L;
+        y = (i / L) % W;
+        z = i / (L * W);
+    }
+};
+
+#endif
