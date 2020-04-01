6db1d2ca86bb882ea89bf07b39fd695472b132c4
andywiecko <andywiecko@gmail.com>
2020-04-01 13:04:39 +0200

    dimensions warning bug fixed

--- CountingMajorana/CountingMajorana_bits/Info/DimensionsWarning.hpp
+++ CountingMajorana/CountingMajorana_bits/Info/DimensionsWarning.hpp
@@ -17,7 +17,7 @@ public:
 
     static void DimensionsWarningOnly2D(int length, int width, int height)
     {
-        if (width > 1 or height > 1)
+        if (height > 1)
         {
             BasicsInfo::Warning("Two dimensional model is selected. ",
                                 "Height value is ignored!");
